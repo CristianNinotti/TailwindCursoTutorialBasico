356dafb56522e4898d77ef037ba083be7c10fa75
Cristian Ninotti <102320892+CristianNinotti@users.noreply.github.com>
2025-08-30 16:19:15 -0300

    Continuacion en youtube de BluuWeb

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Button from "./components/button";
+import Flex from "./components/flex";
 import './App.css'
+
 
 function App() {
 
   return (
     <div>
       <Button />
+      <Flex/>
     </div>
     );
   
---
+++ src/components/Flex.jsx
@@ -0,0 +1,101 @@
+
+
+const Flex = () => {
+    return (
+        <>
+            <div className="container mx-auto mt-5 text-center"></div>
+            <div className="flex gap-2 border-4">
+                <div className="bg-blue-500 p-4 m-2 "></div>
+                <div className="bg-red-500 p-4 m-2 "></div>
+                <div className="bg-green-500 p-4 m-2"></div>
+            </div>
+
+            <div className="container mx-auto mt-5 text-center"></div>
+            <div className="flex flex-col gap-2 border-4">
+                <div className="bg-blue-500 w-20 h-20 "></div>
+                <div className="bg-red-500 w-20 h-20 "></div>
+                <div className="bg-green-500 w-20 h-20 "></div>
+            </div>
+
+            <div className="container mx-auto mt-5 text-center"></div>
+            <div className="flex justify-end flex-wrap border-4">
+                <div className="bg-blue-500 p-4 m-2 "></div>
+                <div className="bg-red-500 p-4 m-2"></div>
+                <div className="bg-green-500 p-4 m-2"></div>
+            </div>
+
+
+            <div className="container mx-auto mt-5 text-center"></div>
+            <div className="flex justify-start flex-wrap border-4">
+                <div className="bg-blue-500 p-4 m-2 "></div>
+                <div className="bg-red-500 p-4 m-2"></div>
+                <div className="bg-green-500 p-4 m-2"></div>
+            </div>
+
+            <div className="container mx-auto mt-5 text-center"></div>
+            <div className="flex justify-between flex-wrap border-4">
+                <div className="bg-blue-500 p-4 m-2 "></div>
+                <div className="bg-red-500 p-4 m-2"></div>
+                <div className="bg-green-500 p-4 m-2"></div>
+            </div>
+
+            <div className="container mx-auto mt-5 text-center"></div>
+            <div className="flex justify-center flex-wrap border-4">
+                <div className="bg-blue-500 p-4 m-2 "></div>
+                <div className="bg-red-500 p-4 m-2"></div>
+                <div className="bg-green-500 p-4 m-2"></div>
+            </div>
+            <div className="container mx-auto mt-5 text-center"></div>
+            <div className="flex justify-around flex-wrap border-4">
+                <div className="bg-blue-500 p-4 m-2 "></div>
+                <div className="bg-red-500 p-4 m-2"></div>
+                <div className="bg-green-500 p-4 m-2"></div>
+            </div>
+            <div className="container mx-auto mt-5 text-center"></div>
+            <div className="flex justify-evenly flex-wrap border-4">
+                <div className="bg-blue-500 p-4 m-2 "></div>
+                <div className="bg-red-500 p-4 m-2"></div>
+                <div className="bg-green-500 p-4 m-2"></div>
+            </div>
+
+            <div className="container mx-auto mt-5 text-center">
+                <div className="flex h-64 items-start flex-wrap border-4">
+                    <div className="bg-blue-500 p-4 m-2 "></div>
+                    <div className="bg-red-500 p-4 m-2"></div>
+                    <div className="bg-green-500 p-4 m-2"></div>
+                </div>
+            </div>
+
+            <div className="container mx-auto mt-5 text-center">
+                <div className="flex h-64 items-end flex-wrap border-4">
+                    <div className="bg-blue-500 p-4 m-2 "></div>
+                    <div className="bg-red-500 p-4 m-2"></div>
+                    <div className="bg-green-500 p-4 m-2"></div>
+                </div>
+            </div>
+
+            <div className="container mx-auto mt-5 text-center">
+                <div className="flex h-64 items-center border-4">
+                    <div className="bg-blue-500 p-4 m-2 "></div>
+                    <div className="bg-red-500 p-4 m-2"></div>
+                    <div className="bg-green-500 p-4 m-2"></div>
+                </div>
+            </div>
+
+
+            <div className="container mx-auto mt-5 text-center">
+            <div className="flex flex-wrap content-center h-64 border-4">
+                <div className="bg-blue-500 p-4 m-2 "></div>
+                <div className="bg-red-500 p-4 m-2"></div>
+                <div className="bg-green-500 p-4 m-2"></div>
+            </div>
+        </div >
+
+        <div className="container mx-auto mt-5 text-center">
+
+        </div>
+        </>
+    )
+}
+
+export default Flex
